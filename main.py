1b9a843dbada2736cb3c6a878fb4829c04e6fdf7
ranuldr24 <ranul.rahinduwage@outlook.com>
2025-05-07 16:11:41 +0530

    06: Moving

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+                
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)  / 1000
-        player.update(dt)
 
 if __name__ == "__main__":
     main()
