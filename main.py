e8fe5c2240e9e2766e5a1963edf818699ef9dbfa
ranuldr24 <ranul.rahinduwage@outlook.com>
2025-05-07 15:30:45 +0530

    05: Moving around.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +18,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)  / 1000
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
